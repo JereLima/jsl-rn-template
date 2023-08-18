163d556c6df794a87efa078158b29f2c0fb0f5b1
Jeremias Santos Lima <69019443+JereLima@users.noreply.github.com>
2023-08-18 14:50:25 -0300

    update archive name

--- template/App.tsx
+++ template/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import {View} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
 
-const App: React.FC = () => {
-  return <View style={{flex: 1, backgroundColor: 'pink'}} />;
-};
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={'red'} />
+      <View style={{}} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '700',
+  },
+});
 
 export default App;
